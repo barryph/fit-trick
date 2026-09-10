1fcb32b7f3e6f7ff52f7ac0155e118108edb819a
Barry Hood <barryph@protonmail.com>
2026-09-10 14:02:01 +1200

    update oauth google-services.json files

--- front-end/app/_layout.tsx
+++ front-end/app/_layout.tsx
@@ -42,7 +42,10 @@ SplashScreen.preventAutoHideAsync();
 // global._fetch = fetch;
 // global.fetch = function (uri, options, ...args) {
 //   return global._fetch(uri, options, ...args).then((response) => {
-//     console.log('Fetch', { request: { uri, options, ...args }, response });
+//     console.log('Fetch:', '\n', { request: { uri, options, ...args } }, '\n', {
+//       response,
+//     });
+//     //console.log('Fetch Response', { response });
 //     return response;
 //   });
 // };
